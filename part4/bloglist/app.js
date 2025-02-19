c91c9684ceebfb445e8ed26342451b7c6abf9fcb
Hebatullah Dwairi <hebamahmoud8843@gmail.com>
2025-02-19 20:18:52 +0300

    4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,7 +5,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
-const blogsRouter = require('./controllers/blogs')
+const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 
 
 mongoose.set('strictQuery', false);
@@ -25,6 +26,7 @@ app.use(cors());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,34 @@
+const usersRouter = require('express').Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+usersRouter.post('/', async (request, response, next) => {
+    try {
+        const { name, username, password } = request.body;
+        const passwordHash = await bcrypt.hash(password, 10);
+        
+        const newUser = new User({
+            name,
+            username,
+            passwordHash,
+        });
+
+        const savedUser = await newUser.save();
+        response.status(201).json(savedUser);
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+usersRouter.get('/', async (request, response, next) => {
+    try {
+        const users = await User.find({});
+        response.json(users);
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+module.exports = usersRouter;
