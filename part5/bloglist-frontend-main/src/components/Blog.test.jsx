32bcaa0144c0774c878fe33e186dc7740b0142c9
Hebatullah Dwairi <hebamahmoud8843@gmail.com>
2025-03-05 10:40:11 +0300

    5.14

--- part5/bloglist-frontend-main/src/components/Blog.test.jsx
+++ part5/bloglist-frontend-main/src/components/Blog.test.jsx
@@ -1,19 +1,40 @@
 import { render, screen } from '@testing-library/react';
 import Blog from './Blog';
+import userEvent from '@testing-library/user-event';
 
 test('renders blog title and author only by default', () => {
-    const blog = {
-        user: '1e5e2357e',
-        title: 'i like potatoes',
-        author: 'falafel',
-        url: 'http://localhost:3001/',
-        likes: '9',
-    }
-    const mockfn = vi.fn()
-    render(<Blog blogs={[blog]} setBlogs={mockfn} index={0} user={{username:'heba', name:'haboosh'}} />);
+  const blog = {
+    user: '1e5e2357e',
+    title: 'i like potatoes',
+    author: 'falafel',
+    url: 'http://localhost:3001/',
+    likes: '9',
+  }
+  const mockfn = vi.fn()
+  render(<Blog blogs={[blog]} setBlogs={mockfn} index={0} user={{username:'heba', name:'haboosh'}} />);
 
-    expect(screen.getByText('i like potatoes', {exact: false})).toBeDefined();
-    expect(screen.getByText('falafel', {exact: false})).toBeDefined();
-    expect(screen.queryByText('http://localhost:3001/', {exact: false})).toBeNull();
-    expect(screen.queryByText('9', {exact: false})).toBeNull();
+  expect(screen.getByText('i like potatoes', {exact: false})).toBeDefined();
+  expect(screen.getByText('falafel', {exact: false})).toBeDefined();
+  expect(screen.queryByText('http://localhost:3001/', {exact: false})).toBeNull();
+  expect(screen.queryByText('9', {exact: false})).toBeNull();
+});
+
+test('number of likes and url are shown when view button is clicked',async () => {
+  const blog = {
+    user: '1e5e2357e',
+    title: 'i like potatoes',
+    author: 'falafel',
+    url: 'http://localhost:3001/',
+    likes: '9',
+  }
+  const mockfn = vi.fn()
+  render(<Blog blogs={[blog]} setBlogs={mockfn} index={0} user={{username:'heba', name:'haboosh'}} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText('view');
+  await user.click(button);
+
+  expect(screen.getByText('http://localhost:3001/', {exact: false})).toBeDefined();
+  expect(screen.getByText('9', {exact: false})).toBeDefined();
+
 });
