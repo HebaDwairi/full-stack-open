f7abcf393bcca2b3d9c885728443149790ebabf8
Hebatullah Dwairi <hebamahmoud8843@gmail.com>
2025-03-27 22:32:22 +0300

    6.10

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,16 +1,15 @@
-const reducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_FILTER': return action.payload;
-    default: return state; 
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    changeFilter(state, action) {
+      return action.payload;
+    }
   }
-}
+});
 
-export const changeFilter = (value) => {
-  return {
-    type: 'SET_FILTER',
-    payload: value,
-  }
-}
-
-
-export default reducer;
+export const { changeFilter } = filterSlice.actions;
+export default filterSlice.reducer;
---
+++ part6/redux-anecdotes/src/reducers/store.js
@@ -0,0 +1,14 @@
+import anecdoteReducer from './anecdoteReducer';
+import filterReducer from './filterReducer';
+import { configureStore } from '@reduxjs/toolkit';
+
+
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer,
+  }
+});
+
+
+export default store;
